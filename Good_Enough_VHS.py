3e86dfa2d54ff93eed4479938529a2b20b27427e
Xaviermw <xmw2@georgetown.edu>
2023-01-14 15:09:58 -0500

    Added Videos and Updated video code

--- Good_Enough_VHS.py
+++ Good_Enough_VHS.py
@@ -12,13 +12,13 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 
-VIDEO_DIRECTORY = "/home/pi/Videos/"
-STATIC_VIDEO = VIDEO_DIRECTORY + "tv static 1080hd 60fps.mp4"
+VIDEO_DIRECTORY = "/home/raspberry/Videos/"
+# STATIC_VIDEO = VIDEO_DIRECTORY + "tv static 1080hd 60fps.mp4"
 BLUE_SCREEN_VIDEO = VIDEO_DIRECTORY + "WellBeRightBack_BlueScreen.mp4"
 
 
 #Setup video destination variable
-video_static = vlc.MediaPlayer(STATIC_VIDEO)
+# video_static = vlc.MediaPlayer(STATIC_VIDEO)
 video_blue_screen = vlc.MediaPlayer(BLUE_SCREEN_VIDEO)
 
 #Make boolean variables
@@ -26,25 +26,25 @@ Running = True
 
 while Running:
 
-
     Tape_Input = GPIO.input(17)
     
-    if  ( Tape_Input == True):
+    if  (Tape_Input == True):
         video_blue_screen.stop()
-        video_static.play()
+        #video_static.play()
         
-        if (video_static.is_playing() == False) :
-            video_static.stop()
-            video_static.play()
-            time.sleep(5)
+#         if (video_static.is_playing() == False) :
+#             video_static.stop()
+#             video_static.play()
+#             time.sleep(5)
         
     else:
-        video_static.stop()
+        print("FALSE")
+#        video_static.stop()
         video_blue_screen.play()
         
         if (video_blue_screen.is_playing() == False) :
-            video_blue_screen.stop()
-            video_blue_screen.play()
-            time.sleep(5)
+             video_blue_screen.stop()
+             video_blue_screen.play()
+             time.sleep(3)
             
             
